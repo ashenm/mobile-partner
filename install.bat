70af5624a3a87f2f705d77cc0cef875d716022d8
Ashen Gunaratne <mail@ashenm.ml>
2017-08-24 21:23:21 +0530

    Reorganized directory structure
    Moved all configuration into a separate folder for easy management.

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ REM Source path
 SET MobilePartnerInstallerPath=%~dp0
 
 REM Determines installation path
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
+IF %PROCESSOR_ARCHITECTURE% == AMD64 (
     SET MobilePartnerRootPath="%ProgramFiles(x86)%\Mobile Partner\"
 ) ELSE (
     SET MobilePartnerRootPath="%ProgramFiles%\Mobile Partner\"
@@ -15,13 +15,15 @@ REM Remove all exisiting language files
 DEL /S /Q /F %MobilePartnerRootPath%plugins\*.lang
 
 REM Copy new configuration
-XCOPY %MobilePartnerInstallerPath%* %MobilePartnerRootPath% /S /R /Y /EXCLUDE:%MobilePartnerInstallerPath%excludes.txt
+XCOPY %MobilePartnerInstallerPath%data\* %MobilePartnerRootPath% /S /R /Y
+
+CLS
 
 REM Conclude execution status to user
-CLS
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO Error - %ERRORLEVEL%
+    ECHO Error: %ERRORLEVEL%
 ) ELSE (
     ECHO Mobile Partner has been successfully optimized!
 )
+
 PAUSE
